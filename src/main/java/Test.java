d5e1c3a827187573d429fe2acf8b2f70869220b7
Esha Jain <esha.x.jain@oracle.com>
2020-05-15 15:11:12 +0530

    first commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,4 +1,12 @@
 public class Test {
+
+    String name;
+
+    public String getName(){
+        name="EJ";
+        return name;
+
+    }
     public static void main(String[] args) {
         System.out.println("hello");
     }
---
+++ src/test/java/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+
+    @org.junit.Test
+    void test(){
+        System.out.println("hello tested");
+    }
+}
